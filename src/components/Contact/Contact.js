35d904cdce1fae365f99ca32bbc07453eaa1075d
Emmanuel Onyeka Eboh <ecolejnr007@gmail.com>
2022-06-13 16:45:59 +0100

    last commit for now

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import AOS from 'aos';
 import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Form from '../Form';
 
 const Contact = () => {
+  useEffect(()=> {
+    AOS.init({ duration: 2500});
+    AOS.refresh();
+  }, []);
+
   return (
     <Section id='contact'> 
       <SectionDivider />
       <br />
-        <SectionTitle>
+        <SectionTitle data-aos='fade-right'>
           Contact Me
         </SectionTitle>
-        <SectionText>
+        <SectionText data-aos='zoom-in-up'>
           Got an offer for me?
           Or a project we can work on? <br />
           Fill in the details and I&apos;ll get back to you as soon as I can.
--- src/components/Contact/ContactStyles.js
+++ src/components/Contact/ContactStyles.js
@@ -3,8 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.form`
     display: flex;
     flex-direction: column;
-    align-items: center;
-    margin: 1rem;
+    margin: 1rem;    
 `
 export const Input = styled.input`
     padding: 2rem;
