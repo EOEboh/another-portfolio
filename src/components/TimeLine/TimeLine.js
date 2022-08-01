285ed8fd8a80509437a805b79414a766c7ff1ee0
Emmanuel Onyeka Eboh <ecolejnr007@gmail.com>
2022-08-01 01:30:15 +0100

    ....

--- src/components/TimeLine/TimeLine.js
+++ src/components/TimeLine/TimeLine.js
@@ -15,8 +15,12 @@ const Timeline = () => {
     <SectionDivider />
     <br />
      <SectionTitle data-aos='fade-right'>About Me</SectionTitle>
-     <SectionText data-aos='zoom-in-up'>I&apos;m a Frontend Software Engineer with {new Date().getFullYear() - 2020} years experience working with various languages and technologies. <br />
-     I play basketball for fun, I listen to music for relaxation.</SectionText>
+     <SectionText data-aos='zoom-in-up'> A Frontend Software Engineer with a talent for solving problems, making in-depth research and writing neat code.  
+     <p> I am that guy you would want in your team, because the value I bring to teamwork is like bread to butter.</p> 
+     <p>
+      I write technical articles and documentations of different technologies and industry trends. Check out my blog below.
+     </p>
+     </SectionText>
 
    </Section>
   );
